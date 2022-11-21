270ce722354640d5a9ddcaf612e68d767f9fe4fb
TommasoBernardo <tommasob1210@gmail.com>
2022-11-21 16:02:37 +0100

    add price discount for minors

--- script.js
+++ script.js
@@ -7,14 +7,32 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 L'output del prezzo finale va emesso (presentato all'utente) in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 */
 
+
+//Dichiaro quanti anni ho
 let age = prompt ("Ciao, per favore inserisci quanti anni hai !");
 console.log(age);
 
+//dichiaro quanti kilometri voglio percorrere
 let kilometers = parseInt ( prompt ("Quanti chilometri vuoi percorrere ?"));
 console.log(kilometers);
 
+//variabili per il prezzo del biglietto
 let ticket;
 let priceKm = 0.21;
 
+//calcolo il prezzo del biglietto
 ticket = kilometers * priceKm ;
-console.log(ticket)
+console.log(ticket);
+
+//variabili sconto 20% per i minorenni e 40% per gli over 65
+let discountMinors = 20 * ticket / 100;
+let discountOver = 40 * ticket /100 
+
+let priceDiscount;
+
+if ( age < 18 ){
+    priceDiscount = ticket - discountMinors;
+    console.log(priceDiscount)
+}
+
+
